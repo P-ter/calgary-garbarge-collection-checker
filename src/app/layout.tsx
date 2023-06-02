0fc019cf71ba7efe1e30f4b65356900b7c52df64
pduong <pduong@streamsystems.ca>
2023-06-02 17:28:12 -0600

    Added analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,26 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import "./globals.css";
+import { Inter } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Calgary Garbage Collection Checker',
-  description: 'Check when your next garbage collection is in Calgary without any complications',
-}
+  title: "Calgary Garbage Collection Checker",
+  description:
+    "Check when your next garbage collection is in Calgary without any complications",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <Analytics />
+      </body>
     </html>
-  )
+  );
 }
